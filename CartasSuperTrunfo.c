b980bfce81f8fe91727f57cea48fe2b3b4856f35
Lucasmotinha2025 <lucas.mota1991@hotmail.com>
2025-02-19 19:55:12 +0000

    Criei as Duas Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 
 int main(){
-    char Est = 'A';
-    char Cod[20] = "A01";
-    char Cid[20] = "Maceió";
-    int Pop = 1388000;
-    float Are = 503.7;
-    float Pi = 274.84;
-    int Pont = 23;
+    char Est1 = 'A';
+    char Est2 = 'B';
+    char Cod1[20] = "A01";
+    char Cod2[20] = "B02";
+    char Cid1[20] = "Macéio";
+    char Cid2[20] = "Belo Horizonte";
+    int Pop1 = 1388000;
+    int Pop2 = 2315560;
+    float Are1 = 503.7;
+    float Are2 = 331.3;
+    float Pi1 = 274.84;
+    float Pi2 = 1058.3;
+    int Pont1 = 23;
+    int Pont2 = 18;
 
     printf("Carta 1:\n");
-    printf("Estado: %c\n", Est);
-    printf("Código: %s\n", Cod);
-    printf("Nome da Cidade: %s\n", Cid);
-    printf("População: %d\n", Pop);
-    printf("Área: %.2f km²\n", Are);
-    printf("PIB: %.2f Bilhões de Reais\n", Pi);
-    printf("Número de Pontos Turísticos: %d\n", Pont);
+    printf("Estado: %c\n", Est1);
+    printf("Código: %s\n", Cod1);
+    printf("Nome da Cidade: %s\n", Cid1);
+    printf("População: %d\n", Pop1);
+    printf("Área: %.2f km²\n", Are1);
+    printf("PIB: %.2f Bilhões de Reais\n", Pi1);
+    printf("Número de Pontos Turísticos: %d\n", Pont1);
 
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", Est2);
+    printf("Código: %s\n", Cod2);
+    printf("Nome da Cidade: %s\n", Cid2);
+    printf("População: %d\n", Pop2);
+    printf("Área: %.2f km²\n", Are2);
+    printf("PIB: %.2f Bilhões de Reais\n", Pi2);
+    printf("Número de Pontos Turísticos: %d\n", Pont2);
+    
     return 0;
 
 }
